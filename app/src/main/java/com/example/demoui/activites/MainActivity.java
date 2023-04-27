5112fafb2bbe66add49b0bb2dce30001015efbd5
sourabh khonde <sourabhkhonde@gmail.com>
2023-04-27 18:39:38 +0530

    Create intro screen and main screen

---
+++ app/src/main/java/com/example/demoui/activites/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.demoui.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demoui.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
