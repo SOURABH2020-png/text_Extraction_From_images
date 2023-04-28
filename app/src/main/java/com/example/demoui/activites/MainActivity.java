57eaadd7d660d70c9b6ac41fcb681701a2a7ac9e
sourabh khonde <sourabhkhonde@gmail.com>
2023-04-28 08:34:18 +0530

    camera open test 1

--- app/src/main/java/com/example/demoui/activites/MainActivity.java
+++ app/src/main/java/com/example/demoui/activites/MainActivity.java
@@ -1,18 +1,62 @@
 package com.example.demoui.activites;
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
 import com.example.demoui.R;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    // Define the pic id
+    private static final int pic_id = 123;
+    Button floatingActionButton;
+    ImageView click_image_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        click_image_id = findViewById(R.id.click_image);
 
+        // Camera_open button is for open the camera and add the setOnClickListener in this button
+//        floatingActionButton.setOnClickListener(v -> {
+//            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
+//            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//            // Start the activity with camera_intent, and request pic id
+//            startActivityForResult(camera_intent, pic_id);
+//        });
     }
+
+    private ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    openCamera();
+                }
+            }
+    );
+    private void openCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(cameraIntent);
+    }
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+
 }
